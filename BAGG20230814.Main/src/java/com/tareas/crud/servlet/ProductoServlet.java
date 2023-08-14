18ed4bf7bb2861f13782ae097563cfc3800da64c
Bagg 12 <bryanalbertoguevaraguerrero@gmail.com>
2023-08-14 08:31:17 -0600

    avance 8:30

---
+++ BAGG20230814.Main/src/java/com/tareas/crud/servlet/ProductoServlet.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java.com.tareas.crud.servlet;
+
+/**
+ *
+ * @author MINEDUCYT
+ */
+public class ProductoServlet {
+    
+}
