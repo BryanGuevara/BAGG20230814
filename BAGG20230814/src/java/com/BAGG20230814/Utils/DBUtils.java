27ca6e7ac00ec52c5d5f63fa84801aaceac4c048
Bagg 12 <bryanalbertoguevaraguerrero@gmail.com>
2023-08-14 11:05:00 -0600

    Avances 11:00 am

--- BAGG20230814/src/java/com/BAGG20230814/Utils/DBUtils.java
+++ BAGG20230814/src/java/com/BAGG20230814/Utils/DBUtils.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package java.com.tareas.crud.dao;
+package com.BAGG20230814.Utils;
 
 /**
  *
  * @author MINEDUCYT
  */
-public class TareaDAO {
+public class DBUtils {
     
 }
